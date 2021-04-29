404cf1da1e302be0f037a0930be11c24c0bac1c5
neverletmeg0 <58898250+neverletmeg0@users.noreply.github.com>
2021-04-29 16:37:05 +0700

    Add files via upload

---
+++ BIGDATA TUGAS.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 29 16:35:37 2021
+
+@author: Never
+"""
+
+print("Nama = Ashar Machruddin\n", "NIM = 04118006\n", "Prodi Sistem Komputer - A\n")
+teks = 'Saya Ashar Mahasiswa Narotama Prodi Sistem Komputer'
